865c8915d997db9e78596e1597f09b6cedc353c0
Bliss Pisit Wetcha <pisit@nimblehq.co>
2022-09-15 19:09:45 +0700

    [#4] Remove jaococo add kover

--- tools/detekt.gradle.kts
+++ tools/detekt.gradle.kts
@@ -1,20 +1,7 @@
-apply {
-    plugin("io.gitlab.arturbosch.detekt")
-}
+// apply(plugin = "io.gitlab.arturbosch.detekt")
 
-detekt {
-    toolVersion = "1.0.0"
-    input = files("src")
-    filters = ".*/resources/.*,.*/build/.*"
-    config = files(file("$project.rootDir/tools/detekt.yml"))
-    reports {
-        xml {
-            enabled = true
-            destination = file("build/reports/detekt/detekt-result.xml")
-        }
-        html {
-            enabled = true
-            destination = file("build/reports/detekt/detekt-result.html")
-        }
-    }
-}
+// detekt {
+//     toolVersion = "1.0.0"
+//     input = files("src")
+//     config = files(file("$project.rootDir/tools/detekt.yml"))
+// }
