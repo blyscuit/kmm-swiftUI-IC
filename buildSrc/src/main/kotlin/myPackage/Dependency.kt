2da5c8054acbc26226e137d731334cf7bd991065
Bliss Pisit Wetcha <pisit@nimblehq.co>
2022-09-19 16:15:40 +0700

    [#8] Add Ktor

--- buildSrc/src/main/kotlin/myPackage/Dependency.kt
+++ buildSrc/src/main/kotlin/myPackage/Dependency.kt
@@ -3,10 +3,12 @@ object Dependency {
     // Kotlin
     const val KOTLIN_GRADLE_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Version.KOTLIN}"
     const val GRADLE = "com.android.tools.build:gradle:${Version.GRADLE}"
+    const val KOTLIN_SERIALIZATION = "org.jetbrains.kotlin:kotlin-serialization:${Version.KOTLIN}"
 
     // Kotlinx
     const val COROUTINES_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Version.COROUTINES_CORE}"
     const val KOVER = "org.jetbrains.kotlinx:kover:${Version.KOVER}"
+    const val KOTLINX_SERIALIZATION = "org.jetbrains.kotlinx:kotlinx-serialization-core:${Version.KOTLINX_SERIALIZATION}"
 
     // DETEKT
     const val DETEKT = "io.gitlab.arturbosch.detekt:detekt-gradle-plugin:${Version.DETEKT}"
@@ -14,10 +16,6 @@ object Dependency {
     // Ktor
     const val KTOR_CORE = "io.ktor:ktor-client-core:${Version.KTOR}"
     const val KTOR_SERIALIZATION = "io.ktor:ktor-client-serialization:${Version.KTOR}"
-    const val KTOR_LOGGING = "io.ktor:ktor-client-logging:${Version.KTOR}"
-    const val KTOR_CIO = "io.ktor:ktor-client-cio:${Version.KTOR}"
-    const val KTOR_CONTENT_NEGOTIATION = "io.ktor:ktor-client-content-negotiation:${Version.KTOR}"
-    const val KTOR_JSON = "io.ktor:ktor-serialization-kotlinx-json:${Version.KTOR}"
     const val KTOR_ANDROID = "io.ktor:ktor-client-android:${Version.KTOR}"
     const val KTOR_IOS = "io.ktor:ktor-client-ios:${Version.KTOR}"
 
