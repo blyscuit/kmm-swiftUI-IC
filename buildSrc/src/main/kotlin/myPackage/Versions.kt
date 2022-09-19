4de2a895f68eda9c4d31d02242166bde6f1f4b85
Bliss Pisit Wetcha <pisit@nimblehq.co>
2022-09-19 15:54:54 +0700

    [#7] Move lib to constants

--- buildSrc/src/main/kotlin/myPackage/Versions.kt
+++ buildSrc/src/main/kotlin/myPackage/Versions.kt
@@ -2,3 +2,13 @@ object Android {
     const val CompileSdk = 32
     const val MinSdk = 26
 }
+
+object Version {
+    const val KOTLIN = "1.7.10"
+    const val GRADLE = "7.2.2"
+    const val COROUTINES_CORE = "1"
+    const val DETEKT = "1.21.0"
+    const val KTOR = "1"
+    const val KOVER = "0.6.0"
+    const val BUILD_KONFIG = "0.13.3"
+}
