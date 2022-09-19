916d39699239d95fa79ecfc1140c976ddd2bf95f
Bliss Pisit Wetcha <pisit@nimblehq.co>
2022-09-19 15:54:54 +0700

    [#8] Move lib to constants

---
+++ buildSrc/src/main/kotlin/myPackage/Dependency.kt
@@ -0,0 +1,26 @@
+object Dependency {
+    
+    // Kotlin
+    const val KOTLIN_GRADLE_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Version.KOTLIN}"
+    const val GRADLE = "com.android.tools.build:gradle:${Version.GRADLE}"
+
+    // Kotlinx
+    const val COROUTINES_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Version.COROUTINES_CORE}"
+    const val KOVER = "org.jetbrains.kotlinx:kover:${Version.KOVER}"
+
+    // DETEKT
+    const val DETEKT = "io.gitlab.arturbosch.detekt:detekt-gradle-plugin:${Version.DETEKT}"
+
+    // Ktor
+    const val KTOR_CORE = "io.ktor:ktor-client-core:${Version.KTOR}"
+    const val KTOR_SERIALIZATION = "io.ktor:ktor-client-serialization:${Version.KTOR}"
+    const val KTOR_LOGGING = "io.ktor:ktor-client-logging:${Version.KTOR}"
+    const val KTOR_CIO = "io.ktor:ktor-client-cio:${Version.KTOR}"
+    const val KTOR_CONTENT_NEGOTIATION = "io.ktor:ktor-client-content-negotiation:${Version.KTOR}"
+    const val KTOR_JSON = "io.ktor:ktor-serialization-kotlinx-json:${Version.KTOR}"
+    const val KTOR_ANDROID = "io.ktor:ktor-client-android:${Version.KTOR}"
+    const val KTOR_IOS = "io.ktor:ktor-client-ios:${Version.KTOR}"
+
+    // BuildKonfig
+    const val BUILD_KONFIG = "com.codingfeline.buildkonfig:buildkonfig-gradle-plugin:${Version.BUILD_KONFIG}"
+}
