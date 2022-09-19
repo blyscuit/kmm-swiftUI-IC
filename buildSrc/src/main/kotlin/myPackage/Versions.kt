2f3274b16b8f13ca67036bd0349fb85a6a626c2f
Bliss Pisit Wetcha <pisit@nimblehq.co>
2022-09-19 16:15:40 +0700

    [#8] Add Ktor

--- buildSrc/src/main/kotlin/myPackage/Versions.kt
+++ buildSrc/src/main/kotlin/myPackage/Versions.kt
@@ -7,9 +7,10 @@ object Android {
 object Version {
     const val KOTLIN = "1.7.10"
     const val GRADLE = "7.2.2"
-    const val COROUTINES_CORE = "1"
+    const val COROUTINES_CORE = "1.5.0-native-mt"
     const val DETEKT = "1.21.0"
-    const val KTOR = "1"
+    const val KTOR = "2.1.1"
     const val KOVER = "0.6.0"
     const val BUILD_KONFIG = "0.13.3"
+    const val KOTLINX_SERIALIZATION = "1.2.2"
 }
