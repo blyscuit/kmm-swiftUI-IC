f9b284d8e43da7a962ab53563203d7b9c9d005b2
Bliss Pisit Wetcha <pisit@nimblehq.co>
2022-09-20 15:42:29 +0700

    [POW] Add example usage for Network layer

--- androidApp/src/main/java/co/nimblehq/blisskmmic/android/MainActivity.kt
+++ androidApp/src/main/java/co/nimblehq/blisskmmic/android/MainActivity.kt
@@ -2,19 +2,36 @@ package co.nimblehq.blisskmmic.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import co.nimblehq.blisskmmic.Greeting
 import android.widget.TextView
+import co.nimblehq.blisskmmic.data.network.core.NetworkClient
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 fun greet(): String {
     return Greeting().greeting()
 }
 
 class MainActivity : AppCompatActivity() {
+    private val mainScope = MainScope()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        mainScope.launch {
+
+            kotlin.runCatching {
+                NetworkClient().getAllLaunches()
+            }.onSuccess {
+                Log.e("", it.title)
+            }.onFailure {
+                Log.e("", it.toString())
+            }
+            tv.text = greet()
+        }
     }
 }
--- iosApp/Survey/Sources/Application/IOSApp.swift
+++ iosApp/Survey/Sources/Application/IOSApp.swift
@@ -6,11 +6,29 @@
 //  Copyright © 2022 Nimble. All rights reserved.
 //
 
+import Combine
 import Shared
 import SwiftUI
+import KMPNativeCoroutinesCombine
 
 @main
 struct IOSApp: App {
+
+    var networkClient = NetworkClient()
+    var cancellables = Set<AnyCancellable>()
+
+    init() {
+        // Create an AnyPublisher for your flow
+        let publisher = createPublisher(for: networkClient.getAllLaunchesNative())
+
+        // Now use this publisher as you would any other
+        publisher.sink { completion in
+            print("Received completion: \(completion)")
+        } receiveValue: { value in
+            print("Received value: \(value)")
+        }.store(in: &cancellables)
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
