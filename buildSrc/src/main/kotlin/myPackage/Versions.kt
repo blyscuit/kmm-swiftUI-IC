f6cbfb3fd88801d9776c6af8e185a8f9b54dbe01
Bliss Pisit Wetcha <pisit@nimblehq.co>
2022-09-20 10:06:16 +0700

    [#8] Add NativeCoroutine class

--- buildSrc/src/main/kotlin/myPackage/Versions.kt
+++ buildSrc/src/main/kotlin/myPackage/Versions.kt
@@ -7,10 +7,11 @@ object Android {
 object Version {
     const val KOTLIN = "1.7.10"
     const val GRADLE = "7.2.2"
-    const val COROUTINES_CORE = "1.5.0-native-mt"
+    const val COROUTINES_CORE = "1.6.4"
     const val DETEKT = "1.21.0"
     const val KTOR = "2.1.1"
     const val KOVER = "0.6.0"
     const val BUILD_KONFIG = "0.13.3"
     const val KOTLINX_SERIALIZATION = "1.2.2"
+    const val NATIVE_COROUTINES_KOTLIN = "0.12.6-new-mm"
 }
