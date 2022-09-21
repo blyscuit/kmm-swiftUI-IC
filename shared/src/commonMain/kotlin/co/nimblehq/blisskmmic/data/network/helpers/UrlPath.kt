85fe19030028b63e74790e3bb528d2bbc9b825f8
Bliss Pisit Wetcha <pisit@nimblehq.co>
2022-09-21 09:35:52 +0700

    [#8] Fix lint

--- shared/src/commonMain/kotlin/co/nimblehq/blisskmmic/data/network/helpers/UrlPath.kt
+++ shared/src/commonMain/kotlin/co/nimblehq/blisskmmic/data/network/helpers/UrlPath.kt
@@ -2,6 +2,6 @@ package co.nimblehq.blisskmmic.data.network.helpers
 
 import co.nimblehq.blisskmmic.BuildKonfig
 
-fun UrlPath(path: String): String {
+fun urlPath(path: String): String {
     return "${BuildKonfig.BASE_URL}${path}"
 }
