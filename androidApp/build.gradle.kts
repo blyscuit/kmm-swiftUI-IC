682c8d4908eeb05fb08080706960947bf4fca33b
Bliss Pisit Wetcha <pisit@nimblehq.co>
2022-09-22 17:53:57 +0700

    [poc] Add android compose example

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     kotlin("android")
+    id("kotlin-android")
 }
 
 android {
@@ -17,6 +18,13 @@ android {
             isMinifyEnabled = false
         }
     }
+    buildFeatures {
+        // Enables Jetpack Compose for this module
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.0"
+    }
 }
 
 dependencies {
@@ -24,4 +32,16 @@ dependencies {
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+
+    // Integration with activities
+    implementation("androidx.activity:activity-compose:1.5.1")
+    // Compose Material Design
+    implementation("androidx.compose.material:material:1.2.1")
+    // Animations
+    implementation("androidx.compose.animation:animation:1.2.1")
+    // Tooling support (Previews, etc.)
+    implementation("androidx.compose.ui:ui-tooling:1.2.1")
+    // Integration with ViewModels
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+
 }
--- androidApp/src/main/java/co/nimblehq/blisskmmic/android/MainActivity.kt
+++ androidApp/src/main/java/co/nimblehq/blisskmmic/android/MainActivity.kt
@@ -2,19 +2,56 @@ package co.nimblehq.blisskmmic.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import co.nimblehq.blisskmmic.Greeting
-import android.widget.TextView
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
+import co.nimblehq.blisskmmic.BreedViewState
+import co.nimblehq.blisskmmic.LoginViewModel
+import co.nimblehq.blisskmmic.data.network.core.NetworkClient
 
 fun greet(): String {
     return Greeting().greeting()
 }
 
 class MainActivity : AppCompatActivity() {
+    val viewModel = LoginViewModel(NetworkClient())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        setContent {
+            MaterialTheme {
+                Column {
+                    GreetingScreen("user1", viewModel)
+                    GreetingScreen("user2", viewModel)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingScreen(
+    userId: String,
+    viewModel: LoginViewModel
+    ) {
+    val state by viewModel.breedState.collectAsState(initial = BreedViewState())
+
+    Text(text = userId)
+    Column {
+        for (name in state.breeds?: listOf("")) {
+            Log.e("", "${state.breeds?.count()}")
+            Text(name)
+        }
     }
 }
