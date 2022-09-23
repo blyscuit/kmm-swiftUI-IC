845bf031c443837057b0f622ebaf0972cf32ba7b
Bliss Pisit Wetcha <pisit@nimblehq.co>
2022-09-23 18:30:28 +0700

    [#9] Refactor viewmodel

--- shared/src/commonMain/kotlin/co/nimblehq/blisskmmic/presentation/modules/login/LoginViewModel.kt
+++ shared/src/commonMain/kotlin/co/nimblehq/blisskmmic/presentation/modules/login/LoginViewModel.kt
@@ -2,11 +2,39 @@ package co.nimblehq.blisskmmic.presentation.modules.login
 
 import co.nimblehq.blisskmmic.domain.model.Token
 import co.nimblehq.blisskmmic.domain.usecase.LogInUseCase
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+data class LoginViewState(
+    val isSuccess: Boolean = false,
+    val isLoading: Boolean = false,
+    val error: String? = null
+) {
+    constructor() : this(false, false, null) {}
+    constructor(error: String?) : this(false, false, error) {}
+}
 
 class LoginViewModel(private val logInUseCase: LogInUseCase) {
 
-    fun login(email: String, password: String) : Flow<Token> {
-        return logInUseCase(email, password)
+    private val mutableViewState: MutableStateFlow<LoginViewState> =
+        MutableStateFlow(LoginViewState())
+    private val viewScope = MainScope()
+
+    val viewState: StateFlow<LoginViewState> = mutableViewState
+
+    fun login(email: String, password: String) {
+        mutableViewState.update {
+            LoginViewState(isLoading = true)
+        }
+        viewScope.launch {
+            logInUseCase(email, password)
+                .catch {
+                    mutableViewState.update { LoginViewState(isLoading = false, error = it.error) }
+                }
+                .collect {
+                    mutableViewState.update { LoginViewState(isSuccess = true, isLoading = false) }
+                }
+        }
     }
 }
