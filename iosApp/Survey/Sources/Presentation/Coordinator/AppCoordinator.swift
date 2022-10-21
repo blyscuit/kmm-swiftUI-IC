df56c1b91fe2c697e655634e98d3c6c25f0f39d2
Bliss Pisit Wetcha <pisit@nimblehq.co>
2022-10-21 12:26:32 +0700

    [#12] Use viewmodel

--- iosApp/Survey/Sources/Presentation/Coordinator/AppCoordinator.swift
+++ iosApp/Survey/Sources/Presentation/Coordinator/AppCoordinator.swift
@@ -6,12 +6,36 @@
 //  Copyright © 2022 Nimble. All rights reserved.
 //
 
+import Combine
 import FlowStacks
+import KMPNativeCoroutinesCombine
+import Shared
 import SwiftUI
 
 struct AppCoordinator: View {
+    class DataSource: ObservableObject {
+        @LazyKoin var viewModel: ResetPasswordViewModel
+        var cancels = Set<AnyCancellable>()
+
+        init() {
+            createPublisher(for: viewModel.viewStateNative)
+                .assertNoFailure()
+                .sink { output in
+                    print(output)
+                    guard let successNotification = output.successNotification else { return }
+                    NotificationManager.current.schedule(
+                        title: successNotification.title,
+                        message: successNotification.message,
+                        time: 0.2
+                    )
+                }
+                .store(in: &cancels)
+            viewModel.reset(email: "pisit@nimblehq.co")
+        }
+    }
 
     @ObservedObject var coordinator = RouteCoordinator()
+    @ObservedObject var dataSource = DataSource()
 
     var body: some View {
         Router($coordinator.routes) { screen, _ in
