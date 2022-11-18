a649a31224b73895db1a4db40b5a7d5bcc7e79ec
Bliss Pisit Wetcha <pisit@nimblehq.co>
2022-11-18 13:08:36 +0700

    [#74] Refactor tests

--- shared/src/commonTest/kotlin/co/nimblehq/blisskmmic/data/network/core/TokenizedNetworkClientTest.kt
+++ shared/src/commonTest/kotlin/co/nimblehq/blisskmmic/data/network/core/TokenizedNetworkClientTest.kt
@@ -1,8 +1,10 @@
 package co.nimblehq.blisskmmic.data.network.core
 
+import co.nimblehq.blisskmmic.BuildKonfig
 import co.nimblehq.blisskmmic.data.database.datasource.LocalDataSource
 import co.nimblehq.blisskmmic.data.database.datasource.MockLocalDataSource
 import co.nimblehq.blisskmmic.data.database.model.TokenDatabaseModel
+import co.nimblehq.blisskmmic.data.network.helpers.API_VERSION
 import co.nimblehq.blisskmmic.helpers.mock.NETWORK_MOCK_MODEL_RESULT
 import co.nimblehq.blisskmmic.helpers.mock.NetworkMockModel
 import co.nimblehq.blisskmmic.helpers.mock.ktor.jsonTokenizedMockEngine
@@ -10,12 +12,14 @@ import co.nimblehq.jsonapi.model.JsonApiException
 import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.shouldBe
 import io.ktor.client.request.*
+import io.ktor.http.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runTest
 import org.kodein.mock.Mocker
 import org.kodein.mock.UsesMocks
+import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.fail
 
@@ -23,20 +27,27 @@ import kotlin.test.fail
 @UsesMocks(LocalDataSource::class)
 class TokenizedNetworkClientTest {
 
-    val token = TokenDatabaseModel(
+    private val mocker = Mocker()
+    private val localDataSource = MockLocalDataSource(mocker)
+
+    private val token = TokenDatabaseModel(
         "Access",
         "",
         1,
         "",
         1
     )
-    val path = "/user"
-    val request = HttpRequestBuilder(path = path)
+    private val path = "user"
+    private val request = HttpRequestBuilder()
+
+    @BeforeTest
+    fun setUp() {
+        mocker.reset()
+        request.url("$BuildKonfig.BASE_URL$API_VERSION$path")
+    }
 
     @Test
     fun `when calling fetch, it returns correct object`() = runTest {
-        val mocker = Mocker()
-        val localDataSource = MockLocalDataSource(mocker)
         mocker.every {
             localDataSource.getToken()
         } returns flow { emit(token) }
@@ -55,8 +66,6 @@ class TokenizedNetworkClientTest {
 
     @Test
     fun `when calling fetch with incorrect token, it returns correct error`() = runTest {
-        val mocker = Mocker()
-        val localDataSource = MockLocalDataSource(mocker)
         mocker.every {
             localDataSource.getToken()
         } returns flow { emit(token) }
