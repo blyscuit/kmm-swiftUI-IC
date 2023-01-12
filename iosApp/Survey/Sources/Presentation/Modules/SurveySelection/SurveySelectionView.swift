687891795db446c3a019db8d3b0f956ca98077fb
Bliss Wetchaya <blyscuit@users.noreply.github.com>
2023-01-12 23:55:29 +0700

    Update SurveySelectionView.swift

--- iosApp/Survey/Sources/Presentation/Modules/SurveySelection/SurveySelectionView.swift
+++ iosApp/Survey/Sources/Presentation/Modules/SurveySelection/SurveySelectionView.swift
@@ -38,11 +38,7 @@ struct SurveySelectionView: View {
 
     var body: some View {
         ZStack {
-            if dataSource.showingLoading {
-                SurveyLoading()
-            } else {
-                survey
-            }
+survey
         }
         .accessibilityElement(children: .contain)
         .accessibility(.surveySelection(.view))
