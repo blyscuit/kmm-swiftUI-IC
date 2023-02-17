78e074a17b9fbbef374474a4891960dc2f65af40
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-02-17 16:42:19 +0700

    [#41] Add viewmodel unit test

--- shared/src/commonTest/kotlin/co/nimblehq/blisskmmic/presentation/modules/surveydetail/SurveyDetailViewModelTest.kt
+++ shared/src/commonTest/kotlin/co/nimblehq/blisskmmic/presentation/modules/surveydetail/SurveyDetailViewModelTest.kt
@@ -1,9 +1,15 @@
+@file:Suppress("MagicNumber")
 package co.nimblehq.blisskmmic.presentation.modules.surveydetail
 
 import app.cash.turbine.test
+import app.cash.turbine.testIn
 import co.nimblehq.blisskmmic.domain.model.SurveyDetail
 import co.nimblehq.blisskmmic.domain.usecase.GetSurveyDetailUseCase
+import co.nimblehq.blisskmmic.domain.usecase.SubmitSurveyUseCase
 import co.nimblehq.blisskmmic.helpers.flow.delayFlowOf
+import co.nimblehq.blisskmmic.presentation.model.SurveySubmissionUiModel
+import co.nimblehq.blisskmmic.presentation.model.toSurveySubmission
+import co.nimblehq.blisskmmic.presentation.models.surveyDetailDummy
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -24,12 +30,18 @@ class SurveyDetailViewModelTest: TestsWithMocks() {
 
     @Mock
     lateinit var getSurveyDetailUseCase: GetSurveyDetailUseCase
+    @Mock
+    lateinit var submitSurveyUseCase: SubmitSurveyUseCase
     @Fake
     lateinit var survey: SurveyDetail
+    @Fake
+    lateinit var answer: SurveySubmissionUiModel.Answer
 
     private val mainThreadSurrogate = newSingleThreadContext("UI thread")
 
-    private val surveyDetailViewModel by withMocks { SurveyDetailViewModel(getSurveyDetailUseCase) }
+    private val surveyDetailViewModel by withMocks {
+        SurveyDetailViewModel(getSurveyDetailUseCase, submitSurveyUseCase)
+    }
 
     override fun setUpMocks() = injectMocks(mocker)
 
@@ -123,7 +135,7 @@ class SurveyDetailViewModelTest: TestsWithMocks() {
     }
 
     @Test
-    fun `When calling getDetail with faliure response - it changes viewState to error`() = runTest {
+    fun `When calling getDetail with failure response - it changes viewState to error`() = runTest {
         surveyDetailViewModel.setSurveyId("")
 
         val errorMessage = "Test Error"
@@ -142,5 +154,88 @@ class SurveyDetailViewModelTest: TestsWithMocks() {
                 result.error shouldBe errorMessage
                 cancel()
             }
+    }
+
+    @Test
+    fun `When calling addAnswer - it changes questionViewState to the correct item`() = runTest {
+        surveyDetailViewModel.setSurveyId("")
+
+        mocker.every {
+            getSurveyDetailUseCase(isAny())
+        } returns delayFlowOf(surveyDetailDummy(3))
+
+        surveyDetailViewModel.getDetail()
+
+        val viewState = surveyDetailViewModel.viewState.testIn(backgroundScope)
+        val questionViewState = surveyDetailViewModel.questionViewState.testIn(backgroundScope)
+        viewState.skipItems(1)
+        viewState.awaitItem()
+        surveyDetailViewModel.addAnswer(listOf(answer))
+        questionViewState.skipItems(1)
+        questionViewState.awaitItem().currentQuestionIndex shouldBe 1
+        surveyDetailViewModel.addAnswer(listOf(answer))
+        questionViewState.awaitItem().currentQuestionIndex shouldBe 2
+        surveyDetailViewModel.addAnswer(listOf(answer))
+        val lastState = questionViewState.awaitItem()
+        lastState.currentQuestionIndex shouldBe 3
+        lastState.isShowingSubmit shouldBe true
+        viewState.cancelAndIgnoreRemainingEvents()
+        questionViewState.cancelAndIgnoreRemainingEvents()
+    }
+
+    @Test
+    fun `When calling submitAnswer with success response - it calls submitSurveyUseCase with correct arguments and returns correct values`() = runTest {
+        surveyDetailViewModel.setSurveyId("")
+
+        val submissions = listOf(SurveySubmissionUiModel("", listOf(answer)))
+
+        mocker.every {
+            getSurveyDetailUseCase(isAny())
+        } returns delayFlowOf(surveyDetailDummy(3))
+        mocker.every {
+            submitSurveyUseCase(submissions.toSurveySubmission(""))
+        } returns delayFlowOf(Unit)
+
+        surveyDetailViewModel.getDetail()
+
+        val viewState = surveyDetailViewModel.viewState.testIn(backgroundScope)
+        val questionViewState = surveyDetailViewModel.questionViewState.testIn(backgroundScope)
+        viewState.skipItems(1)
+        viewState.awaitItem()
+        surveyDetailViewModel.addAnswer(listOf(answer))
+        questionViewState.skipItems(2)
+        surveyDetailViewModel.submitAnswer()
+        questionViewState.awaitItem().isLoading shouldBe true
+        questionViewState.awaitItem().isShowingSuccess shouldBe true
+        viewState.cancelAndIgnoreRemainingEvents()
+        questionViewState.cancelAndIgnoreRemainingEvents()
+    }
+
+    @Test
+    fun `When calling submitAnswer with error response - it returns correct error`() = runTest {
+        surveyDetailViewModel.setSurveyId("")
+
+        val errorMessage = "Test Error"
+
+        mocker.every {
+            getSurveyDetailUseCase(isAny())
+        } returns delayFlowOf(surveyDetailDummy(3))
+        mocker.every {
+            submitSurveyUseCase(isAny())
+        } returns delayFlowOf(errorMessage)
+
+        surveyDetailViewModel.getDetail()
+
+        val viewState = surveyDetailViewModel.viewState.testIn(backgroundScope)
+        val questionViewState = surveyDetailViewModel.questionViewState.testIn(backgroundScope)
+        viewState.skipItems(1)
+        viewState.awaitItem()
+        surveyDetailViewModel.addAnswer(listOf(answer))
+        questionViewState.skipItems(2)
+        surveyDetailViewModel.submitAnswer()
+        questionViewState.awaitItem().isLoading shouldBe true
+        questionViewState.awaitItem().isShowingSuccess shouldBe false
+        viewState.cancelAndIgnoreRemainingEvents()
+        questionViewState.cancelAndIgnoreRemainingEvents()
     }
 }
