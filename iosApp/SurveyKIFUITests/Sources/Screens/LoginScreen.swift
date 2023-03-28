76e9e7e097418a44586a704ce287d7622a37c9b4
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-03-28 12:54:04 +0700

    [Chore] Temp add more wait

--- iosApp/SurveyKIFUITests/Sources/Screens/LoginScreen.swift
+++ iosApp/SurveyKIFUITests/Sources/Screens/LoginScreen.swift
@@ -17,11 +17,12 @@ final class LoginScreen: GenericScreen {
         if tester().tryFindingView(withAccessibilityIdentifier: ViewId.splash(.view)()) {
             tester().waitForAbsenceOfView(withAccessibilityIdentifier: ViewId.splash(.view)())
         }
-        if tester().tryFindingView(withAccessibilityIdentifier: ViewId.login(.view)()) {
+        while tester().tryFindingView(withAccessibilityIdentifier: ViewId.login(.view)()) {
+            system().wait(forTimeInterval: 10.0)
             tester().waitForTappableView(withAccessibilityIdentifier: ViewId.login(.loginButton)())
             tester().tapView(withAccessibilityIdentifier: ViewId.login(.loginButton)())
-            tester().waitForAbsenceOfView(withAccessibilityIdentifier: ViewId.login(.view)())
         }
+        tester().waitForAbsenceOfView(withAccessibilityIdentifier: ViewId.login(.view)())
     }
 
     func waitForAppearance() {
